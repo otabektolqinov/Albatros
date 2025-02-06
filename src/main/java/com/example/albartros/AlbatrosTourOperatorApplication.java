d8b77f201aedf57e91b1a747d386f9f9651da766
Tolqinov Otabek <otabektolqinov30@gmail.com>
2025-02-06 14:49:46 +0500

    Project set up

---
+++ src/main/java/com/example/albartros/AlbatrosTourOperatorApplication.java
@@ -0,0 +1,13 @@
+package com.example.albartros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlbatrosTourOperatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlbatrosTourOperatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/albartros/AlbatrosTourOperatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.albartros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlbatrosTourOperatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
